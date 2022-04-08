48f4268ac2354b40436430c98d494148f950ece2
Programazing <chris@thatamazingprogrammer.com>
2022-04-08 16:28:36 -0400

    Add CSS to JS

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-let csvURL = 'https://raw.githubusercontent.com/Programazing/CoffeeChatVolunteers/dev/volunteers.csv'
-let loadCSV = Papa.parse(csvURL, {
-    download: true,
-    quotes: false,
-    delimiter: ",",
-    header: true,
-    complete: function(results) {
-        generateVolunteerCard(results)
-    }
-})
+var papaParse = document.createElement('script')
+papaParse.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.js')
+papaParse.setAttribute('crossorigin','anonymous')
+papaParse.setAttribute('referrerpolicy','no-referrer')
+document.head.appendChild(papaParse)
 
-function generateVolunteerCard(results) {
-    for(item of results.data){
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-        document.body.append(card)
+var css = document.createElement('link')
+css.setAttribute('rel','stylesheet')
+css.href = 'https://raw.githubusercontent.com/Programazing/CoffeeChatVolunteers/index.css'
+document.head.appendChild(css);
 
-        const a = document.createElement('a')
-        a.setAttribute('target', '_blank')
-        a.href = item.link
+window.onload = () => {
+    let csvURL = 'https://raw.githubusercontent.com/Programazing/CoffeeChatVolunteers/volunteers.csv'
+    Papa.parse(csvURL, {
+        download: true,
+        quotes: false,
+        delimiter: ",",
+        header: true,
+        complete: function(results) {
+            generateVolunteerCard(results)
+        }
+    })
 
-        const img = document.createElement('img')
-        img.setAttribute('alt', 'Avatar')
-        img.src = item.image
+    function generateVolunteerCard(results) {
+        for(item of results.data){
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+            document.body.append(card)
 
-        const container = document.createElement('div')
-        container.setAttribute('class', 'container')
+            const a = document.createElement('a')
+            a.setAttribute('target', '_blank')
+            a.href = item.link
 
-        const name = document.createElement('h4')
-        name.append(item.name)
+            const img = document.createElement('img')
+            img.setAttribute('alt', 'Avatar')
+            img.src = item.image
 
-        a.append(img)
-        container.append(name)
-        a.append(container)
-        card.appendChild(a)
-    }  
+            const container = document.createElement('div')
+            container.setAttribute('class', 'container')
+
+            const name = document.createElement('h4')
+            name.append(item.name)
+
+            a.append(img)
+            container.append(name)
+            a.append(container)
+            card.appendChild(a)
+        }  
+    } 
 }
